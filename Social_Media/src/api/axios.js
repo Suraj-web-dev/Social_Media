816c4f979b47aa3af6e7e7f98f4ea0082eeba5b8
Suraj Gautam <jayshriram9356@gmail.com>
2026-09-22 23:52:20 +0530

    Fix CORS and API URL

--- Social_Media/src/api/axios.js
+++ Social_Media/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://social-media-iv1q-56sz5sb2j-suraj-a081.vercel.app/api",
   withCredentials: true,
 });
 
